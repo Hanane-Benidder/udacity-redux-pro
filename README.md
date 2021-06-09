bf7b76536a7da3b89a36465d5ff9aacbf2f235fc
Hanan-Bennider <BeniDDer1Hanane2@gmail.com>
2021-06-10 00:45:01 +0100

    fix readme

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Redux_Project
+
 # Getting Started with Create React App
 
 This project was created with [Create React App]
@@ -15,15 +16,31 @@ Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
 ## Technologies used:
+
 > React
 
 > Redux
 
 > React Router
 
-
 ## Usage
 
 The home page contains all questions categorized into "Answered" and "Unanswered".
 By default a user is shown all unanswered questions first and then can navigate to answered questions by selecting the approriate link. Each question links to the question detail page. Here, a user can vote by selection an option. Vote results are shown immmediately with the user's response,
 the total number and percentage of votes for each option. A logged in user can vote on an option only once.
+
+> Home Page :
+
+shows two different sections : answered and unanswered .
+
+> Create New Question :
+
+is used to ask and submit a new polls .
+
+> Questions Details :
+
+is used to answered a question or vie answers of auther users .
+
+> LeaderBoard :
+
+shows total number of answered and unanswered questions of all users    .
--- src/component/login.js
+++ src/component/login.js
@@ -1,11 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Login.css";
 import { users } from "../utils/_DATA";
 import { Redirect } from "react";
 import { setLoginUser } from "../actions/loginUser";
 import { connect } from "react-redux";
-
-class loginPage extends Component {
+function login() {
   state = {
     userId: "",
     mainpage: false,
@@ -24,46 +23,36 @@ class loginPage extends Component {
       this.props.dispatch(setLoginUser(this.state.userId));
     });
   };
+  const { userId, mainpage } = this.state;
+  const photo = "https://clipground.com/images/redux-logo-8.jpg";
+  return (
+    <div className="parentdiv">
+      <h3> Welcome to Would You Rather App!</h3>
+      <img src={photo} />
+      <form onSubmit={this.handlelogin}>
+        {console.log("cliked" + " " + userId)}
+        <select
+          className="btn"
+          value={this.state.userId}
+          onChange={this.handleselection}
+        >
+          <option value="" disabled defaultValue hidden>
+            Choose A User
+          </option>
 
-  render() {
-    const { userId, mainpage } = this.state;
-    const photo = "https://clipground.com/images/redux-logo-8.jpg";
-
-    return (
-      <div className="parentdiv">
-        <h3> Welcome to Would You Rather App!</h3>
-        <img src={photo} />
-        <form onSubmit={this.handlelogin}>
-          {console.log("cliked user " + userId)}
-          <select
-            className="btn"
-            value={this.state.userId}
-            onChange={this.handleselection}
-          >
-            <option value="" disabled defaultValue hidden>
-              Choose A User
-            </option>
-
-            {Object.keys(users).map((user) => {
-              return (
-                <option value={users[user].id} key={user}>
-                  {users[user].name}
-                </option>
-              );
-            })}
-          </select>
-          <button className="btn" type="submit" disabled={userId === ""}>
-            login
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
-function mapStateToProps({ users, loginUser }) {
-  return {
-    users: Object.keys(users),
-    loginUser,
-  };
+          {Object.keys(users).map((user) => {
+            return (
+              <option value={users[user].id} key={user}>
+                {users[user].name}
+              </option>
+            );
+          })}
+        </select>
+        <button className="btn" type="submit" disabled={userId === ""}>
+          login
+        </button>
+      </form>
+    </div>
+  );
 }
 export default connect(mapStateToProps)(loginPage);
